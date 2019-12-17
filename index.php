f753860fae78dfff0eab70441c806397463371d5
Kunshi <rude1979@gmail.com>
2019-12-17 15:16:00 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$data = "見出し" . "ZZZ";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Gcalendar</title>
+  <link rel="stylesheet" href="./style.css" type="text/css">
+</head>
+<body>
+<h1 class="">Gcalendar</h1>
+
+<a class="button" href="/">Google認証</a>
+
+<h1 id="">3<?php echo $data; ?></h1>
+</form>
+</body>
+</html>
